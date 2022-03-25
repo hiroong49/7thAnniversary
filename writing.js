e03e21d3d816ee766fdccbcd09fc45fa4c694e26
Jimeaning <kjimin63@naver.com>
2022-03-25 18:11:15 +0900

    개발: 누른 아이콘만 선명하게 보이기

--- writing.js
+++ writing.js
@@ -1,16 +1,23 @@
-document.getElementById("next_btn").disabled = true;
-const icon = document.getElementsByTagName('button');
 
-// 아이콘 선택하면 다음으로 버튼 활성화
+
+// 아이콘 선택하면 꾹 눌리고 다음으로 버튼 활성화
 function select_icon() {
+    var btns = document.getElementsByClassName("btn"); 
+
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            if(current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+            this.className += " active"; // 선택되면 클래스 이름 active 추가 
+        });
+    }
+
+    // 다음으로 버튼 활성화 
     document.getElementById("next_btn").disabled = false;
-    // 해당 컨테이너 색으로 구분
-    changeBorder();
 }
 
-function changeBorder() {
-    icon.style.color = 'lightgray';
-}
 
 // 다음으로 버튼 이벤트 처리 
 function to_letter() {
