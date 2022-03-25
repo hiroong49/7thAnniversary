037a47527a38415351aae23c2f7d77ffb154a751
pinus0711 <yyis3173@gmail.com>
2022-03-25 13:06:20 +0900

    letter html

---
+++ database/connect/maria.js
@@ -0,0 +1,2 @@
+const maria=require('mysql')
+
--- letter.js
+++ letter.js
@@ -1,12 +1,25 @@
-const uploadBtn=document.querySelector('.image-upload');
-const realInput=document.querySelector('#real-input');
+const uploadBtn=document.querySelector('.imageUpload');
+const realInput=document.querySelector('#realInput');
 
 uploadBtn.addEventListener('click',()=>{
     realInput.click();
 })
 
-function readInputFile(e){
-    var sel_file=[];
+function readFile(event){
+    var reader=new FileReader();
+
+    reader.onload=function(event){
+        var img=document.getElementById("imagePreview");
+        img.src=event.target.result;
+        
+    };
+
+    reader.readAsDataURL(this.files[0]);
 
     
 }
+
+const inputImage = document.getElementById("realInput")
+inputImage.addEventListener("change", e => {
+    readImage(e.target);
+})
