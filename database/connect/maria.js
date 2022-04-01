a12ac9f3b5cff45842e5d926a1a22b7c6e98dd54
pinus0711 <yyis3173@gmail.com>
2022-04-02 01:19:07 +0900

    letter 기능구현 및 디자인

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -1,2 +1,12 @@
-const maria=require('mysql')
+const http=require('http');
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const mysql=require('mysql');
 
+app.use(bodyParser.urlencoded({extended:false}));
+
+const mariadb=require('mariadb');
+const pool=mariadb.createPool({
+
+});
--- letter_writing/letter.js
+++ letter_writing/letter.js
@@ -1,25 +1,12 @@
-const uploadBtn=document.querySelector('.imageUpload');
-const realInput=document.querySelector('#realInput');
 
-uploadBtn.addEventListener('click',()=>{
-    realInput.click();
-})
 
 function readFile(event){
     var reader=new FileReader();
 
-    reader.onload=function(event){
-        var img=document.getElementById("imagePreview");
-        img.src=event.target.result;
-        
+    reader.onload=function(e){
+        var preview=document.querySelector("#preview");
+        preview.src=e.target.result;
     };
-
-    reader.readAsDataURL(this.files[0]);
-
-    
+       reader.readAsDataURL(event.target.files[0]);
 }
 
-const inputImage = document.getElementById("realInput")
-inputImage.addEventListener("change", e => {
-    readImage(e.target);
-})
